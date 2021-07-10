80ff6434ece8ba233a74612a0a2f4963c8d85d73
mehonator <mehonator@yandex.ru>
2021-07-10 20:56:59 +0300

    переделал доступ к вьюхам, через слаг, чтобы через него искать  соответсвующие таблицы

--- employee_time_sheet/urls.py
+++ employee_time_sheet/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from employee_time_sheet.views import (
     TablesUchetaRabochegoVremeniDetail,
     TablesUchetaRabochegoVremeniList,
+    table_ucheta_rabochego_vremeni_create,
 )
 
 app_name = "employee_time_sheet"
 urlpatterns = [
     path(
-        "tables_ucheta_rabochego_vremeni_list/",
+        "<slug:unit_organization>/tables_ucheta_rabochego_vremeni_list/",
         TablesUchetaRabochegoVremeniList.as_view(),
         name="tables_ucheta_rabochego_vremeni_list",
     ),
@@ -15,5 +16,10 @@ urlpatterns = [
         "tables_ucheta_rabochego_vremeni_detail/<int:pk>/",
         TablesUchetaRabochegoVremeniDetail.as_view(),
         name="tables_ucheta_rabochego_vremeni_detail",
+    ),
+    path(
+        "<slug:unit_organization>/tables_ucheta_rabochego_vremeni_create/",
+        table_ucheta_rabochego_vremeni_create,
+        name="table_ucheta_rabochego_vremeni_create",
     ),
 ]
