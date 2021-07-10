67d7c1e3c43d3ac23bd616010e07376b480a36a9
mehonator <mehonator@yandex.ru>
2021-07-10 20:59:31 +0300

    кастомный класс, тк надо было пометить slug как read_only

--- logistics/admin.py
+++ logistics/admin.py
@@ -7,7 +7,13 @@ from logistics.models import (
     UnitOrganization,
 )
 
-admin.site.register(UnitOrganization)
+
+class UnitOrganizationAdmin(admin.ModelAdmin):
+    fields = ("name", "slug")
+    readonly_fields = ("slug",)
+
+
+admin.site.register(UnitOrganization, UnitOrganizationAdmin)
 admin.site.register(Product)
 admin.site.register(Delevery)
 admin.site.register(Profile)
