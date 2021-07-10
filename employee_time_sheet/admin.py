5b9ff225465739fc40baf96a8c91c37655e59076
mehonator <mehonator@yandex.ru>
2021-07-10 20:51:48 +0300

    забыл добавить некоторые модели в админку

--- employee_time_sheet/admin.py
+++ employee_time_sheet/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from employee_time_sheet.models import (
     Row,
     Table,
+    Day,
 )
 
-admin.site.register(Row)
+
 admin.site.register(Table)
+admin.site.register(Row)
+admin.site.register(Day)
