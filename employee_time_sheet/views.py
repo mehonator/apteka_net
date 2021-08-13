1bcdd29bc48d994243eeb9483a6e8f45b54208ce
mehonator <mehonator@yandex.ru>
2021-08-13 17:25:47 +0300

    add choose staff view

--- employee_time_sheet/views.py
+++ employee_time_sheet/views.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List, Dict, Tuple
 from django.contrib.auth import get_user_model
 from django.forms.formsets import BaseFormSet
 from django.forms.models import inlineformset_factory, modelformset_factory
@@ -16,7 +16,7 @@ from employee_time_sheet.forms import (
     ChooseStaffForm,
 )
 from employee_time_sheet.models import Day, Row, Table
-from logistics.models import UnitOrganization
+from logistics.models import Profile, UnitOrganization
 
 
 User = get_user_model()
@@ -75,25 +75,28 @@ def validate_staff_form(parsed_form):
     return True
 
 
-ChooseStaffFormset = formset_factory(ChooseStaffForm, extra=0)
+def get_init_data_choose_staf(staff, is_choosen: bool) -> List[Dict]:
+    staff_init = []
+    for one_of_staff in staff:
+        initial_data = {
+            "pk_staff": one_of_staff.pk,
+            "full_name": one_of_staff.get_full_name,
+            "is_choosen": is_choosen,
+        }
+
+        staff_init.append(initial_data)
+    return staff_init
 
 
 def tables_add_in_unit(request, unit_organization):
+    ChooseStaffFormset = formset_factory(ChooseStaffForm, extra=0)
     if request.method == "GET":
         unit_organization = get_object_or_404(
             UnitOrganization, slug=unit_organization
         )
-        staff = unit_organization.profiles_staff.all()
-        staff_init = []
-        for one_of_staff in staff:
-            initial_data = {
-                "pk_staff": one_of_staff.pk,
-                "full_name": one_of_staff.user.get_full_name,
-                "choosen": True,
-            }
-
-            staff_init.append(initial_data)
-
+        staff_profile = unit_organization.profiles_staff.all()
+        staff = User.objects.filter(profile__in=staff_profile)
+        staff_init = get_init_data_choose_staf(staff, is_choosen=True)
         choose_staff_formset = ChooseStaffFormset(initial=staff_init)
         context = {
             "unit_organization": unit_organization,
@@ -148,14 +151,12 @@ def index(request):
         )
 
 
-FullTable = inlineformset_factory(
-    Table,
-    Row,
-    fields=("staff",),
-)
-
-
 def table_ucheta_rabochego_vremeni_create(request):
+    FullTable = inlineformset_factory(
+        Table,
+        Row,
+        fields=("staff",),
+    )
     if request.method == "POST":
         table_form = TabbleForm(request.POST)
         row_form = RowForm(request.POST)
@@ -240,3 +241,66 @@ def detail_formset(request, pk: int):
             "table_ucheta_rabochego_vremeni_detail formset.html",
             context=context,
         )
+
+
+def get_chosen_and_unchosen(staff_data: List[Dict]) -> Tuple:
+    pks_chosen = []
+    pks_unchosen = []
+    for one_of_staff in staff_data:
+        if one_of_staff["is_choosen"]:
+            pks_chosen.append(one_of_staff["pk_staff"])
+        else:
+            pks_unchosen.append(one_of_staff["pk_staff"])
+    chosens = User.objects.filter(pk__in=pks_chosen)
+    unchosens = User.objects.filter(pk__in=pks_unchosen)
+    return chosens, unchosens
+
+
+def add_rows(users, table):
+    for user in users:
+        Row.objects.update_or_create(staff=user, table=table)
+
+
+def del_rows(users, table):
+    Row.objects.filter(staff__in=users, table=table).delete()
+
+
+def choose_staff(request, pk: int):
+    ChooseStaffFormset = formset_factory(ChooseStaffForm, extra=0)
+    if request.method == "GET":
+        table = get_object_or_404(Table, pk=pk)
+
+        selected_staff = User.objects.filter(
+            row_table_ucheta_rabochego_vremeni__in=table.rows.all()
+        )
+        selected_profile = Profile.objects.filter(user__in=selected_staff)
+        unselected_staff = User.objects.filter(
+            profile__units_organizations=table.unit_organization
+        ).exclude(profile__in=selected_profile)
+
+        init_data_staff = get_init_data_choose_staf(
+            selected_staff, is_choosen=True
+        )
+        init_data_staff += get_init_data_choose_staf(
+            unselected_staff, is_choosen=False
+        )
+
+        choose_staff_formset = ChooseStaffFormset(initial=init_data_staff)
+        context = {"choose_staff_formset": choose_staff_formset}
+        return render(
+            request, "employee_time_sheet/tables_choose_staff.html", context
+        )
+
+    elif request.method == "POST":
+        input_choose_staff_formset = ChooseStaffFormset(request.POST)
+        if input_choose_staff_formset.is_valid():
+            table = get_object_or_404(Table, pk=pk)
+            chosens, unchosens = get_chosen_and_unchosen(
+                input_choose_staff_formset.cleaned_data
+            )
+            add_rows(chosens, table)
+            del_rows(unchosens, table)
+
+            return HttpResponseRedirect(
+                reverse("employee_time_sheet:detail", kwargs={"pk": pk})
+            )
